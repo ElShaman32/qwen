cf36913639312b1a506902a3b695f39e0896df3b
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-08-31 22:10:57 -0400

    Delete Lib/core directory

--- Lib/core/database/tables/historial_tasa_table.dart
+++
@@ -1,9 +0,0 @@
-import 'package:drift/drift.dart';
-
-/// Histórico de tasas de cambio para auditoría y reportes.
-class HistorialTasa extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  RealColumn get tasa => real()();
-  TextColumn get fuente => text().withDefault(const Constant('manual'))();
-  DateTimeColumn get fecha => dateTime().withDefault(currentDateAndTime)();
-}
