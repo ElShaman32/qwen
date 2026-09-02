8513ea1abcd2cce01d22584b6dc278db2017a0c2
ElShaman32 <123756162+ElShaman32@users.noreply.github.com>
2026-09-02 01:50:40 -0400

    Delete Lib/core directory

--- Lib/core/services/client_firebase_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'client_firebase.dart';
-
-/// Provider del wrapper ClientFirebase.
-/// Expone la instancia singleton del servicio.
-final clientFirebaseProvider = Provider<ClientFirebase>((ref) {
-  return ClientFirebase();
-});
